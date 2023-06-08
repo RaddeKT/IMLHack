74642606e9d761916152a6361d6fb96859c6e13c
Yosef Abraham <yo.ab@outlook.com>
2023-06-08 19:23:24 +0300

    cateogies using pd

--- 1/code/hackathon_code/preprocess.py
+++ 1/code/hackathon_code/preprocess.py
@@ -30,17 +30,41 @@ CATEGORY_COLUMNS_TO_EXPAND = [col for col in CATEGORY_COLUMNS if
                               col not in DROPPED_COLUMNS]
 
 
+# func that returns a dummified DataFrame of significant dummies in a given column
+def limit_categories(df, column_names):
+    threshold = 0.1
+
+    final_df = df.copy()
+
+    final_df[column_names] = final_df[column_names].astype('str')
+
+    for column in column_names:
+        # removes the bind
+        column_vals = final_df[column]
+
+        # what is the ratio of a dummy in whole column
+        count = pd.value_counts(column_vals) / len(column_vals)
+
+        # cond whether the ratios is higher than the threshold
+        mask = column_vals.isin(count[count > threshold].index)
+
+        # replace the ones which ratio is lower than the threshold by a special name
+        column_vals[~mask] = "OTHERS"
+
+        final_df[column] = column_vals
+
+    return final_df
+
+
 def categorize_columns(df, column_names):
     final_df = df.copy()
     final_df[column_names] = final_df[column_names].astype('str')
-    encoder = OneHotEncoder(max_categories=4, sparse_output=False)
-    data = encoder.fit_transform(final_df[column_names])
-    final_df[encoder.get_feature_names_out()] = pd.DataFrame(data, columns=encoder.get_feature_names_out())
-    # for column_name in column_names:
-    #     # convert to category using get_dummies
-    #     final_df = pd.get_dummies(final_df, columns=[column_name], prefix=f'cat_{column_name}', prefix_sep='_')
 
-    final_df = final_df.drop(column_names, axis=1)
+    for column_name in column_names:
+        # convert to category using get_dummies
+        final_df = pd.get_dummies(final_df, columns=[column_name], prefix=f'cat_{column_name}', prefix_sep='_')
+
+    # final_df = final_df.drop(column_names, axis=1)
     return final_df
 
 
@@ -79,11 +103,11 @@ def parse_cancellation_policy(df):
     return final_df
 
 
-def replace_empty_with_UNKNOWN(df, column_names):
+def replace_empty_with_OTHERS(df, column_names):
     final_df = df.copy()
     for column_name in column_names:
         # Replace the empty values with UNKNOWN
-        final_df[column_name] = final_df[column_name].fillna("UNKNOWN")
+        final_df[column_name] = final_df[column_name].fillna("OTHERS")
 
     return final_df
 
@@ -183,19 +207,19 @@ def get_preprocessed_data(df, task=1, is_test=False):
 
     # Handle categorical columns
     df = (df
-          .pipe(replace_empty_with_UNKNOWN, CATEGORY_COLUMNS_TO_EXPAND)
+          .pipe(replace_empty_with_OTHERS, CATEGORY_COLUMNS_TO_EXPAND)
+          .pipe(limit_categories, CATEGORY_COLUMNS_TO_EXPAND)
           .pipe(categorize_columns, CATEGORY_COLUMNS_TO_EXPAND)
           )
 
     df = df.pipe(parse_cancellation_policy)
 
     df = df.drop(DATE_COLUMNS + DATETIME_COLUMNS, axis=1)
+    df = df.drop(['policy2_days_before_checkin'], axis=1)
     if not is_test:
         df = df.drop(['cancellation_datetime'], axis=1)
 
     df = df.fillna(df.mean())
-
-    # TODO: Handle currency payment
 
     return df
 
