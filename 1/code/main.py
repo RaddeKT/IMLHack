042d62e9c6475fbc090eda21df1c7d8e5f121338
RaddekT <daniel.ageev76@gmail.com>
2023-06-08 17:43:35 +0300

    Changed preprocessing

--- 1/code/main.py
+++ 1/code/main.py
@@ -1,44 +1,51 @@
 import pandas as pd
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.metrics import f1_score, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+from hackathon_code.preprocess import get_preprocessed_data
 
 
 def execute_task_1(train_df, test_df):
     ids = test_df["h_booking_id"]
 
-    clean_train = preprocess(train_df)
-    clean_test = preprocess(test_df, is_test=True)
+    clean_train = get_preprocessed_data(train_df, task=1)
+    clean_test = get_preprocessed_data(test_df, task=1, is_test=True).reindex(clean_train.columns, axis=1, fill_value=0)
+    clean_test.drop("did_cancel", axis=1, inplace=True)
 
     X_train, y_train = clean_train.drop("did_cancel", axis=1), clean_train["did_cancel"]
 
-    model = LinearDiscriminantAnalysis()
+    model = RandomForestClassifier()
     model.fit(X_train, y_train)
     y_pred = model.predict(clean_test)
 
     result_df = pd.DataFrame({"h_booking_id": ids, "did_cancel": y_pred})
-    result_df.to_csv("../predictions/task1.csv", index=False)
+    result_df.to_csv("predictions/task1.csv", index=False)
 
 
 def execute_task_2(train_df, test_df):
     ids = test_df["h_booking_id"]
 
-    clean_train = preprocess(train_df)
-    clean_test = preprocess(test_df, is_test=True)
+    clean_train = get_preprocessed_data(train_df, task=1)
+    clean_test = get_preprocessed_data(test_df, task=1, is_test=True).reindex(clean_train.columns, axis=1, fill_value=0)
+    clean_test.drop("did_cancel", axis=1, inplace=True)
 
-    X_train, y_train = clean_train.drop("original_selling_amount", axis=1), clean_train["original_selling_amount"]
+    X_train, y_train = clean_train.drop("cancellation_date", axis=1), clean_train["cancellation_date"]
 
-    model = LinearRegression()
+    model = RandomForestClassifier()
     model.fit(X_train, y_train)
     y_pred = model.predict(clean_test)
 
-
     result_df = pd.DataFrame({"h_booking_id": ids, "predicted_selling_amount": y_pred})
-    result_df.to_csv("../predictions/task2.csv", index=False)
+    result_df.to_csv("predictions/task1.csv", index=False)
 
 
 def main():
-    task1_train = pd.read_csv("../datasets/agoda_cancellation_train.csv")
-    task1_test = pd.read_csv("../datasets/Agoda_Test_1.csv")
+    task1_train = pd.read_csv("datasets/agoda_cancellation_train.csv")
+    task1_test = pd.read_csv("datasets/Agoda_Test_1.csv")
     execute_task_1(task1_train, task1_test)
 
     task2_train = pd.read_csv("../datasets/agoda_cancellation_train.csv")
